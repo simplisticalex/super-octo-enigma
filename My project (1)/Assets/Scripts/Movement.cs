31c2a6779c1821f6edbb9f39624f37b359dd5ece
Alex <141527321+simplisticalex@users.noreply.github.com>
2026-02-01 12:40:04 +0300

    aaa
    короче сам разбирайся со своими анимациями

--- My project (1)/Assets/Scripts/Movement.cs
+++ My project (1)/Assets/Scripts/Movement.cs
@@ -1,21 +1,58 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1.0f;
-    private Rigidbody rb;
-    private Vector3 moveDir;
-    void Awake()
+    [Header("Settings")]
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _turnSpeed = 10f;
+
+    private CharacterController _controller;
+    private Vector2 _moveInput;
+    private Vector3 _currentVelocity;
+    private Animator _animator;
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
-    void Update()
+
+    public void OnMove(InputAction.CallbackContext context)
     {
-        moveDir.x = Input.GetAxis("Horizontal");
-        moveDir.z = Input.GetAxis("Vertical"); ;
+        _moveInput = context.ReadValue<Vector2>();
     }
-    void FixedUpdate()
+
+    private void Update()
     {
-        rb.MovePosition(rb.position + moveSpeed * moveDir * Time.fixedDeltaTime);
+        MovePlayer();
+        float horizontalSpeed = new Vector3(_moveInput.x, 0, _moveInput.y).magnitude;
+        _animator.SetFloat("Speed", horizontalSpeed);
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 moveDirection = new Vector3(_moveInput.x, 0, _moveInput.y).normalized;
+
+        if (moveDirection.magnitude >= 0.1f)
+        {
+
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _turnSpeed * Time.deltaTime);
+
+            _controller.Move(moveDirection * _speed * Time.deltaTime);
+        }
+
+        if (!_controller.isGrounded)
+        {
+            _currentVelocity.y += Physics.gravity.y * Time.deltaTime;
+        }
+        else
+        {
+            _currentVelocity.y = -0.5f;
+        }
+
+        _controller.Move(_currentVelocity * Time.deltaTime);
     }
 }
